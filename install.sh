66696a467c52f89c572bd23c4caa466088f24197
Ryan Curtis <rbrcurtis@gmail.com>
2012-09-15 21:12:33 -0400

    key mappings

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@
 cd ~/Library/Application\ Support/Sublime\ Text\ 2/Packages
 git clone git@github.com:rbrcurtis/dev-env.git Custom
 rm User/Preferences.sublime-settings
+rm User/Default\ \(OSX\).sublime-keymap
 
+ln -s ../Custom/Default\ \(OSX\).sublime-keymap User/Default\ \(OSX\).sublime-keymap
 ln -s ../Custom/Preferences.sublime-settings User/Preferences.sublime-settings
